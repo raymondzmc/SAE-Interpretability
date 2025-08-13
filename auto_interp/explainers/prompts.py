3f51531c315bf7e604ba4f6d0fee57efd952d0a6
Raymond Li <li.raymond96@gmail.com>
2025-08-13 15:56:29 -0700

    import error

--- auto_interp/explainers/prompts.py
+++ auto_interp/explainers/prompts.py
@@ -206,3 +206,57 @@ def system(cot=False):
             "content": SYSTEM.format(prompt=prompt),
         }
     ]
+
+
+
+def build_examples(
+    **kwargs,
+):
+    examples = []
+
+    for i in range(1, 4):
+        prompt, response = example(i, **kwargs)
+
+        messages = [
+            {
+                "role": "user",
+                "content": prompt,
+            },
+            {
+                "role": "system",
+                "content": response,
+            },
+        ]
+
+        examples.extend(messages)
+
+    return examples
+
+
+def build_prompt(
+    examples,
+    activations: bool = False,
+    cot: bool = False,
+):
+    
+    messages = system(
+        cot=cot,
+    )
+
+    few_shot_examples = build_examples(
+        activations=activations,
+        cot=cot,
+    )
+
+    messages.extend(few_shot_examples)
+
+    user_start = f"\n{examples}\n"
+
+    messages.append(
+        {
+            "role": "user",
+            "content": user_start,
+        }
+    )
+
+    return messages
