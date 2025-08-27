1d35be2945ca0254ca6bc67b6c1fec973347ef03
Raymond Li <li.raymond96@gmail.com>
2025-08-27 00:12:12 -0700

    imp

--- scripts/run_lagrangian_hardconcrete_softplus_with_lb_sweep.sh
+++ scripts/run_lagrangian_hardconcrete_softplus_with_lb_sweep.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 conda activate sae
 
-# CUDA_VISIBLE_DEVICES=0 python run_experiments.py \
-# --base_config configs/tinystories/tinystories-lagrangian-hardconcrete.yaml \
-# --sweep_config configs/tinystories/sweep/lagrangian_hardconcrete/softplus_sweep_with_lb.yaml \
-# --output_dir experiment_outputs/softplus_lagrangian_hardconcrete_sweep_with_lb
+CUDA_VISIBLE_DEVICES=0 python run_experiments.py \
+--base_config configs/tinystories/tinystories-lagrangian-hardconcrete.yaml \
+--sweep_config configs/tinystories/sweep/lagrangian_hardconcrete/softplus_sweep_with_lb.yaml \
+--output_dir experiment_outputs/softplus_lagrangian_hardconcrete_sweep_with_lb
 
 CUDA_VISIBLE_DEVICES=0 python evaluation.py \
 --wandb_project raymondl/tinystories-1m-lagrangian-hardconcrete \
