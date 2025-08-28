2c262e145c5444c06459f74eed0dc8fec19a28b2
Raymond Li <li.raymond96@gmail.com>
2025-08-28 00:03:57 -0700

    hc new

--- scripts/run_hardconcrete_sweep.sh
+++ scripts/run_hardconcrete_sweep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 conda activate sae
 
-CUDA_VISIBLE_DEVICES=0 python run_experiments.py \
+CUDA_VISIBLE_DEVICES=1 python run_experiments.py \
 --base_config configs/tinystories/tinystories-hardconcrete.yaml \
 --sweep_config configs/tinystories/sweep/hardconcrete_sweep.yaml \
 --output_dir experiment_outputs/hardconcrete_sweep
 
-CUDA_VISIBLE_DEVICES=0 python evaluation.py \
+CUDA_VISIBLE_DEVICES=1 python evaluation.py \
 --wandb_project raymondl/tinystories-1m-hardconcrete \
 --filter_runs_by_name standard_hardconcrete
