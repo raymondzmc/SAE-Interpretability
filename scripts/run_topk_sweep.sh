8a7f7948b7a1617ea138c73f14324af3fba07b1c
Raymond Li <li.raymond96@gmail.com>
2025-09-02 22:45:06 -0700

    updates

--- scripts/run_topk_sweep.sh
+++ scripts/run_topk_sweep.sh
@@ -2,11 +2,11 @@
 #!/bin/bash
 conda activate sae
 
-CUDA_VISIBLE_DEVICES=0 python run_experiments.py \
+CUDA_VISIBLE_DEVICES=1 python run_experiments.py \
 --base_config configs/tinystories-test/tinystories-topk.yaml \
 --sweep_config configs/tinystories-test/sweep/topk_sweep.yaml \
 --output_dir experiment_outputs/topk_sweep
 
-CUDA_VISIBLE_DEVICES=0 python evaluation.py \
+CUDA_VISIBLE_DEVICES=1 python evaluation.py \
 --wandb_project raymondl/tinystories-1m-test \
 --filter_runs_by_name topk
