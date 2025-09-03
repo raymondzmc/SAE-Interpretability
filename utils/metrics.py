dad810244aea350ddfafa1a3d9347a5344f1d3ac
Raymond Li <li.raymond96@gmail.com>
2025-09-03 12:11:19 -0700

    updates

--- utils/metrics.py
+++ utils/metrics.py
@@ -159,7 +159,7 @@ def loss_metrics(output: SAETransformerOutput) -> dict[str, float]:
     loss_metrics = {}
     for name, loss_output in output.loss_outputs.items():
         for loss_name, loss_value in loss_output.loss_dict.items():
-            loss_metrics[f"loss/{loss_name}/{name}"] = loss_value.item()
+            loss_metrics[f"loss/{loss_name}/{name}"] = loss_value if isinstance(loss_value, float) else loss_value.item()
     return loss_metrics
 
 
